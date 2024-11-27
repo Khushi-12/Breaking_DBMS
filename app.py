495ff1ad2f264d3be71b3ed7be1c3321d4fdb4c0
Khushi-12 <nhushikeema12@gmaill.com>
2024-11-27 12:22:31 -0500

    Authentication added to the login page

--- app.py
+++ app.py
@@ -14,6 +14,17 @@ def intro():
 
     return render_template('login.html')
 
+@app.route('/login',methods = ['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if username == 'admin' and password == 'password123':
+        return jsonify({"message":"Valid Credentials"}), 200
+    else:
+        return jsonify({"message":"Invalid Credentials, Login Unsuccessful"}), 401
+
 
 @app.route('/login',methods = ['POST'])
 def login():
