5ce94f3a4f8f797c950ec8d96e349733881e32c5
ShaneHolmes10 <48338528+ShaneHolmes10@users.noreply.github.com>
2024-11-28 17:31:40 -0500

    Cleaned up the code in the user function

--- app.py
+++ app.py
@@ -32,16 +32,6 @@ def user():
 
     customers = con.query("SELECT * FROM chemical_database.customer;")
     
-    '''[
-        {'name' : 'yo', 'email' : 'eep'},
-        {'name' : 'nope', 'email' : 'sleep'},
-        {'name' : 'hell', 'email' : 'meep'}
-    ]'''
-
-    cus = con.query("SELECT * FROM chemical_database.customer;")
-
-    print(cus[0]['first_name'])
-
     return render_template('pharmacy.html', customers=customers)
 
 
